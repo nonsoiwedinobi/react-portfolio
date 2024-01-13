39ca2d57a302d48e2b4eabf9c9957384df820873
Nonso Iwedinobi <nonsoiwedinobi@yahoo.com>
2024-01-13 00:38:49 +0000

    Error fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,16 +4,25 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-    return (
-        <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'm Nonso</h1>
-            <p className={styles.description}>I'm a front-end developer with 1 year experience using HTML, CSS, JavaScript, React and NodeJS. Contact me if you'd like to hire me.</p>
-            <a className={styles.contactBtn} href="/assets/NonsoIwedinobiDev.pdf" target="blank">Download CV</a>
-        </div>
-        <img className={styles.heroImg} src={getImageUrl("hero/profile.jpg")} alt="Hero image of me" />
-        <div className={styles.topBlur} />
-        <div className={styles.bottomBlur} />
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Nonso</h1>
+        <p className={styles.description}>
+          I'm a front-end developer with 1 year experience using HTML, CSS,
+          JavaScript, React and NodeJS. Contact me if you'd like to hire me.
+        </p>
+        <a className={styles.contactBtn} href="/assets/NonsoIwedinobiDev.pdf">
+          Download CV
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/profile.jpg")}
+        alt="Hero image of me"
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
-    );
+  );
 };
